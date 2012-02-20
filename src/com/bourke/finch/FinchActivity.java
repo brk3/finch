12270fd8f129a71b4d5aabb834b89e3d3c263ef6
Paul Bourke <pauldbourke@gmail.com>
2012-02-20 00:23:09 +0000

    Add AsyncTask to get credentials with oauth
    Nearly working, still some network operations that need to be moved off the
    main thread (currently throwing exception due to this).

--- src/com/bourke/finch/FinchActivity.java
+++ src/com/bourke/finch/FinchActivity.java
@@ -1,29 +1,55 @@
 package com.bourke.finch;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+
+import android.net.Uri;
+
 import android.os.Bundle;
+
+import android.preference.PreferenceManager;
 
 import android.support.v4.app.ActionBar;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.view.Menu;
 import android.support.v4.view.MenuItem;
 
+import android.util.Log;
+
+import android.webkit.WebView;
+
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.bourke.finch.lazylist.LazyAdapter;
 import com.bourke.finch.lazylist.TestAdapter;
 
 import twitter4j.auth.AccessToken;
+import twitter4j.auth.RequestToken;
+
 import twitter4j.Twitter;
+
+import twitter4j.TwitterException;
+
 import twitter4j.TwitterFactory;
+import android.os.AsyncTask;
 
 public class FinchActivity extends FragmentActivity
         implements ActionBar.OnNavigationListener {
 
-    private static final String CONSUMER_KEY = "7QIjyQd3cA8c8jn80tRtqw";
-    private static final String CONSUMER_SECRET =
+    private static final String TAG = "Finch/FinchActivity";
+
+    protected static final String CONSUMER_KEY = "7QIjyQd3cA8c8jn80tRtqw";
+    protected static final String CONSUMER_SECRET =
         "yREZDaGBZfIGnlNGce9m80jRUKbnkYhZGT7XZkFZqg";
+    protected static final String CALLBACK_URL = "finch-callback:///";
+
+	private static final String PREF_ACCESS_TOKEN = "accessToken";
+	private static final String PREF_ACCESS_TOKEN_SECRET = "accessTokenSecret";
+    private SharedPreferences mPrefs;
 
     public static int THEME_DARK = R.style.Theme_Finch;
     public static int THEME_LIGHT = R.style.Theme_Finch_Light;
@@ -33,10 +59,21 @@ public class FinchActivity extends FragmentActivity
     private ListView mMainList;
     private LazyAdapter mMainListAdapter;
 
+    private Twitter mTwitter;
+	private RequestToken mReqToken;
+
+    private Context mContext;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
+
+        mContext = getApplicationContext();
+
+		/* Load the twitter4j helper */
+		mTwitter = new TwitterFactory().getInstance();
+		mTwitter.setOAuthConsumer(CONSUMER_KEY, CONSUMER_SECRET);
 
         /* Set layout and theme */
         setTheme(THEME_LIGHT);
@@ -60,15 +97,67 @@ public class FinchActivity extends FragmentActivity
         //adapter = new LazyAdapter(this, mStrings);
         //mMainList.setAdapter(adapter);
 
-        /* Check for saved log in details */
-        //checkForSavedLogin();
-
-        /* Set consumer and provider on teh Application service */
-        //getConsumerProvider();
+        /* Setup prefs and check if credentials present */
+        mPrefs = getSharedPreferences("twitterPrefs", MODE_PRIVATE);
+		if (mPrefs.contains(PREF_ACCESS_TOKEN)) {
+			Log.d(TAG, "Repeat User");
+			loginAuthorisedUser();
+		} else {
+			Log.d(TAG, "New User");
+            new AuthTask().execute();
+		}
 
         /* Set actionbar subtitle to user's username */
         getSupportActionBar().setSubtitle("@brk3");
     }
+
+	@Override
+	protected void onNewIntent(Intent intent) {
+		super.onNewIntent(intent);
+
+		dealWithTwitterResponse(intent);
+	}
+
+    /*
+     * The user had previously given our app permission to use Twitter.
+     * Therefore we retrieve these credentials and fill out the Twitter4j
+     * helper.
+	 */
+	private void loginAuthorisedUser() {
+		String token = mPrefs.getString(PREF_ACCESS_TOKEN, null);
+		String secret = mPrefs.getString(PREF_ACCESS_TOKEN_SECRET, null);
+
+		AccessToken at = new AccessToken(token, secret);
+		mTwitter.setOAuthAccessToken(at);
+
+		Toast.makeText(this, "Welcome back", Toast.LENGTH_SHORT).show();
+	}
+
+	private void dealWithTwitterResponse(Intent intent) {
+		Uri uri = intent.getData();
+
+        try {
+            if (uri != null && uri.toString().startsWith(CALLBACK_URL)) {
+                /* Authorise twitter client with user credentials */
+                String oauthVerifier = uri.getQueryParameter("oauth_verifier");
+                AccessToken at =
+                    mTwitter.getOAuthAccessToken(mReqToken, oauthVerifier);
+                mTwitter.setOAuthAccessToken(at);
+
+                /* Save creds to preferences for future use */
+                String token = at.getToken();
+                String secret = at.getTokenSecret();
+                SharedPreferences.Editor editor = mPrefs.edit();
+                editor.putString(PREF_ACCESS_TOKEN, token);
+                editor.putString(PREF_ACCESS_TOKEN_SECRET, secret);
+                editor.commit();
+
+                Toast.makeText(this, "Logged In", Toast.LENGTH_SHORT).show();
+            }
+        } catch (TwitterException e) {
+            e.printStackTrace();
+        }
+	}
 
     @Override
     public boolean onNavigationItemSelected(int itemPosition, long itemId) {
@@ -93,6 +182,37 @@ public class FinchActivity extends FragmentActivity
                     MenuItem.SHOW_AS_ACTION_WITH_TEXT);
 
         return true;
+    }
+
+    private class AuthTask extends AsyncTask<String, String, String> {
+
+        @Override
+        protected void onPreExecute() {
+        }
+
+        @Override
+        protected String doInBackground(String... urls) {
+
+            String authUrl = "";
+            try {
+                Log.i(TAG, "Request App Authentication");
+                mReqToken = mTwitter.getOAuthRequestToken(CALLBACK_URL);
+                authUrl = mReqToken.getAuthenticationURL();
+
+            } catch (TwitterException e) {
+                e.printStackTrace();
+                return null;
+            }
+            return authUrl;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            if (!result.equals("")) {
+                startActivity(new Intent(Intent.ACTION_VIEW,
+                            Uri.parse(result)));
+            }
+        }
     }
 
 }
