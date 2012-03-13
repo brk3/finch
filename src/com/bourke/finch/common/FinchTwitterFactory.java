bbe84a6198a83175901ea9a4140457fef4600dda
Paul Bourke <pauldbourke@gmail.com>
2012-03-13 23:22:02 +0000

    Enable HttpResponseCache
    Needs testing but definitely appears to speed things up.

--- src/com/bourke/finch/common/FinchTwitterFactory.java
+++ src/com/bourke/finch/common/FinchTwitterFactory.java
@@ -2,7 +2,11 @@ package com.bourke.finch.common;
 
 import android.content.Context;
 
+import android.util.Log;
+
 import com.bourke.finch.common.Constants;
+
+import java.io.File;
 
 import twitter4j.conf.Configuration;
 import twitter4j.conf.ConfigurationBuilder;
@@ -24,6 +28,8 @@ public class FinchTwitterFactory {
     private FinchTwitterFactory(Context context) {
         mContext = context;
 
+        installHttpResponseCache();
+
         ConfigurationBuilder configurationBuilder = new ConfigurationBuilder();
         configurationBuilder.setOAuthConsumerKey(Constants.CONSUMER_KEY);
         configurationBuilder.setOAuthConsumerSecret(Constants.CONSUMER_SECRET);
@@ -37,6 +43,34 @@ public class FinchTwitterFactory {
             singletonInstance = new FinchTwitterFactory(context);
         }
         return singletonInstance;
+    }
+
+    /**
+     * Setup the Android 4.0 HttpResponseCache if on ICS or higher, otherwise
+     * fall back to compatibility lib.
+     */
+    private void installHttpResponseCache() {
+        final long httpCacheSize = 5 * 1024 * 1024; // 5 MiB
+        final File httpCacheDir = new File("/sdcard", "http");
+
+        try {
+            Class.forName("android.net.http.HttpResponseCache")
+                .getMethod("install", File.class, long.class)
+                .invoke(null, httpCacheDir, httpCacheSize);
+        } catch (Exception httpResponseCacheNotAvailable) {
+            Log.d(TAG, "android.net.http.HttpResponseCache not available, " +
+                    "probably because we're running on a pre-ICS version of " +
+                    "Android. Using com.integralblue.httpresponsecache." +
+                    "HttpHttpResponseCache.");
+            try {
+                com.integralblue.httpresponsecache.HttpResponseCache.install(
+                        httpCacheDir, httpCacheSize);
+            } catch(Exception e) {
+                Log.e(TAG, "Failed to set up " +
+                       "com.integralblue.httpresponsecache.HttpResponseCache");
+                e.printStackTrace();
+            }
+        }
     }
 
     public Twitter getTwitter() {
